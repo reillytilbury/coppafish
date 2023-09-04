30459ad5d4876418911f5a0a9a460bbe0e629084
Reilly <reillytilbury@gmail.com>
2023-09-04 14:45:48 +0100

    median tile updated to closest tile in use to median

--- coppafish/pipeline/call_reference_spots.py
+++ coppafish/pipeline/call_reference_spots.py
@@ -133,10 +133,14 @@ def call_reference_spots(config: dict, nbp_file: NotebookPage, nbp_basic: Notebo
                                                     gene_efficiency=ge_initial), use_channels, nbp_basic.n_channels)
     nbp.gene_efficiency = gene_efficiency
 
+    # Find middle tile to calculate intensity threshold
+    median_tile = int(np.median(nbp_basic.use_tiles))
+    dist = np.linalg.norm(nbp_basic.tilepos_yx - nbp_basic.tilepos_yx[median_tile], axis=1)[nbp_basic.use_tiles]
+    central_tile = nbp_basic.use_tiles[np.argmin(dist)]
     # Backward compatibility here as OMP requires nbp.abs_intensity_percentile.
     rc_ind = np.ix_(nbp_basic.use_rounds, nbp_basic.use_channels)
     pixel_colors = get_spot_colors(all_pixel_yxz(nbp_basic.tile_sz, nbp_basic.tile_sz, nbp_basic.nz // 2),
-                                   int(np.median(nbp_basic.use_tiles)), transform, nbp_file, nbp_basic,
+                                   central_tile, transform, nbp_file, nbp_basic,
                                    return_in_bounds=True)[0]
     pixel_intensity = get_spot_intensity(np.abs(pixel_colors) / nbp.color_norm_factor[rc_ind])
     nbp.abs_intensity_percentile = np.percentile(pixel_intensity, np.arange(1, 101))
