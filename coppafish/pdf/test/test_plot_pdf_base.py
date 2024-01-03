b7fc7790fc46eacd6f9707eb73d7d805a30e3a6b
Paul Shuker <paul.shuker@outlook.com>
2024-01-03 15:47:39 +0000

    moved output path for pdf build

--- coppafish/pdf/test/test_plot_pdf_base.py
+++ coppafish/pdf/test/test_plot_pdf_base.py
@@ -8,9 +8,12 @@ from coppafish import BuildPDF
 def test_BuildPDF():
     robominnie_notebook = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
-        "../../../robominnie/test/integration_dir/output_coppafish/notebook.npz",
+        "../../robominnie/test/integration_dir/output_coppafish/notebook.npz",
     )
+    output_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "unit_test_dir/diagnostics.pdf")
+    if not os.path.isdir(os.path.dirname(output_path)):
+        os.mkdir(output_path)
     if not os.path.isfile(robominnie_notebook):
         assert False, f"Could not find robominnie notebook at\n\t{robominnie_notebook}.\nRun an integration test first"
 
-    BuildPDF(robominnie_notebook, auto_open=False)
+    BuildPDF(robominnie_notebook, output_path=output_path, auto_open=False)
