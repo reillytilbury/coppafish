c6f14b8b815edaae4d19cc27e9d57088df86d981
Paul Shuker <paul.shuker@outlook.com>
2024-01-17 14:49:20 +0000

    new indexing functions + unit test

---
+++ coppafish/utils/test/test_utils_indexing.py
@@ -0,0 +1,13 @@
+from coppafish.utils import indexing
+
+
+def test_unique():
+    assert indexing.unique([(0,), (1,), (2,)]) == [(0,), (1,), (2,)]
+    assert indexing.unique([(0,), (0,), (2,)]) == [(0,), (2,)]
+    assert indexing.unique([(0,), (1,), (2,)], 0) == [(0,), (1,), (2,)]
+    assert indexing.unique([(0,), (0,), (1,)], 0) == [(0,), (1,)]
+    assert indexing.unique([(1,), (1,), (1,)], 0) == [(1,)]
+    assert indexing.unique([(0, 0), (1, 0), (2, 1)], 1) == [(0, 0), (2, 1)]
+
+
+test_unique()
