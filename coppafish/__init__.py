faaf08b1ea4015846f03aacf872f23c1dabb7bba
Paul Shuker <paul.shuker@outlook.com>
2024-02-05 10:05:11 +0000

    removed un-needed importerror check for plot functions

--- coppafish/__init__.py
+++ coppafish/__init__.py
@@ -4,10 +4,6 @@ from .pipeline.run import run_pipeline
 from .setup import Notebook, NotebookPage
 from .utils.pciseq import export_to_pciseq
 from ._version import __version__
-try:
-    from . import plot
-    from .plot import Viewer
-    from .pdf.base import BuildPDF
-except ModuleNotFoundError:
-    # So no error if not installed napari/matplotlib etc
-    pass
+from . import plot
+from .plot import Viewer
+from .pdf.base import BuildPDF
