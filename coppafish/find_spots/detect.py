62c00ceb7c6a1f5630c2ab0c40e9f6451de35909
Paul Shuker <paul.shuker@outlook.com>
2024-02-23 09:49:57 +0000

    detect spots does int -> int32 when possible to save memory

--- coppafish/find_spots/detect.py
+++ coppafish/find_spots/detect.py
@@ -14,28 +14,28 @@ def detect_spots(
     se: Optional[np.ndarray] = None,
 ) -> Tuple[np.ndarray, np.ndarray]:
     """
-    Finds local maxima in image exceeding ```intensity_thresh```.
+    Finds local maxima in image exceeding `intensity_thresh`.
     This is achieved through a dilation being run on the whole image.
     Should use for a large se.
 
     Args:
-        image: ```float [n_y x n_x x n_z]```.
-            ```image``` to find spots on.
-        intensity_thresh: Spots are local maxima in image with ```pixel_value > intensity_thresh```.
+        image: `float [n_y x n_x x n_z]`.
+            `image` to find spots on.
+        intensity_thresh: Spots are local maxima in image with `pixel_value > intensity_thresh`.
         radius_xy: Radius of dilation structuring element in xy plane (approximately spot radius).
         radius_z: Radius of dilation structuring element in z direction (approximately spot radius).
             Must be more than 1 to be 3D.
-            If ```None```, 2D filter is used.
+            If `None`, 2D filter is used.
         remove_duplicates: Whether to only keep one pixel if two or more pixels are local maxima and have
             same intensity. Only works with integer image.
-        se: ```int [se_sz_y x se_sz_x x se_sz_z]```.
+        se: `int [se_sz_y x se_sz_x x se_sz_z]`.
             Can give structuring element manually rather than using a cuboid element.
             Must only contain zeros and ones.
 
     Returns:
-        - ```peak_yxz``` - ```int [n_peaks x image.ndim]```.
+        - `peak_yxz` - `int [n_peaks x image.ndim]`.
             yx or yxz location of spots found.
-        - ```peak_intensity``` - ```float [n_peaks]```.
+        - `peak_intensity` - `float [n_peaks]`.
             Pixel value of spots found.
     """
     if se is None:
@@ -80,6 +80,8 @@ def detect_spots(
 
     consider_intensity = image[consider_yxz]
     consider_yxz = np.array(consider_yxz)
+    if consider_yxz.max() <= np.iinfo(np.int32).max:
+        consider_yxz = consider_yxz.astype(np.int32)
 
     logging.debug(f"{consider_yxz.shape=}")
     paddings = np.array([pad_size_y, pad_size_x, pad_size_z])[: image.ndim]
@@ -89,7 +91,7 @@ def detect_spots(
     else:
         peak_intensity = consider_intensity[keep]
     peak_yxz = consider_yxz.transpose()[keep]
-    return peak_yxz, peak_intensity
+    return peak_yxz.astype(int), peak_intensity
 
 
 def get_local_maxima(
