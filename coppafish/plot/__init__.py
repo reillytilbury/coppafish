97d17da0af25fb8f95135e3458cc2d0859c3b038
Reilly <reillytilbury@gmail.com>
2024-05-28 00:02:17 +0100

    added stitch diagnostics

--- coppafish/plot/__init__.py
+++ coppafish/plot/__init__.py
@@ -4,7 +4,6 @@ from .call_spots import view_scaled_k_means
 from .call_spots.dot_product import view_score
 from .call_spots.weight import view_weight
 from .call_spots.background import view_background
-from .stitch import view_stitch_shift_info, view_stitch_search, view_stitch_overlap, view_stitch
 from .raw import view_raw
 from .extract import view_extracted_images
 from .filter import view_filtered_images
