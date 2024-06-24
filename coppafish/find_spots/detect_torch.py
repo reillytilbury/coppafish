f7b49d2c5308bac2901f1cb96e5f733053a0e243
DrExplosive <paul.shuker@outlook.com>
2024-06-24 13:29:44 +0100

    removed numpy detect spots + better unit testing

--- coppafish/find_spots/detect_torch.py
+++ coppafish/find_spots/detect_torch.py
@@ -5,7 +5,6 @@ import numpy as np
 import torch
 
 from .. import utils
-from .. import log
 
 
 def detect_spots(
@@ -18,8 +17,9 @@ def detect_spots(
     force_cpu: bool = True,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     """
-    Finds local maxima in image exceeding `intensity_thresh`. This is achieved through a dilation being run on the
-    whole image.
+    Finds local maxima in image exceeding `intensity_thresh`. If multiple local maxima are found within the given
+    radius local region, then the most intense maxima is taken. If the maxima are the same intensity, then one is
+    chosen at random.
 
     Args:
         - image (`(n_y x n_x x n_z) tensor[Any]`): `image` to find spots on.
@@ -28,8 +28,8 @@ def detect_spots(
             length in y/x will be 2 * radius_xy - 1.
         - radius_z (int): radius of dilation structuring element in z direction (approximately spot radius). The total
             length in z will be 2 * radius_z - 1.
-        - remove_duplicates: whether to only keep one pixel if two or more pixels are local maxima and have same
-            intensity. Only works with integer image.
+        - remove_duplicates: keep one pixel if two or more pixels are nearby local maxima and have same intensity.
+            Default: false.
         - se: `int [se_sz_y x se_sz_x x se_sz_z]`.
             can give structuring element manually rather than using a cuboid element.
             Must only contain zeros and ones.
@@ -50,6 +50,8 @@ def detect_spots(
     assert se is None or type(se) is torch.Tensor
     assert type(force_cpu) is bool
 
+    # Image is mutable, so create image_temp.
+    image_temp = image.detach().clone()
     se = np.ones((2 * radius_xy - 1, 2 * radius_xy - 1, 2 * radius_z - 1), dtype=int)
     pad_size_y = radius_xy - 1
     pad_size_x = radius_xy - 1
@@ -58,18 +60,20 @@ def detect_spots(
     se[np.ix_(*[(np.floor((se.shape[i] - 1) / 2).astype(int),) for i in range(se.ndim)])] = 0
     se_shifts = torch.asarray(np.array(utils.morphology.filter.get_shifts_from_kernel(se)))
 
-    consider_yxz = (image > intensity_thresh).nonzero(as_tuple=True)
+    consider_yxz = (image_temp > intensity_thresh).nonzero(as_tuple=True)
+    consider_intensity = image_temp[consider_yxz]
+    consider_intensity_random = consider_intensity.detach().clone()
     n_consider = consider_yxz[0].shape[0]
     if remove_duplicates:
-        image = image.to(dtype=torch.float32)
-        # perturb image by small amount so two neighbouring pixels that did have the same value now differ slightly.
-        # hence when find maxima, will only get one of the pixels not both.
+        image_temp = image_temp.to(dtype=torch.float32)
+        consider_intensity_random = consider_intensity_random.to(image_temp.dtype)
+        # To avoid nearby duplicates
         rng = np.random.default_rng(0)  # So shift is always the same.
         # rand_shift must be larger than small to detect a single spot.
-        rand_im_shift = torch.asarray(rng.uniform(low=2e-6, high=0.2, size=n_consider), dtype=image.dtype)
-        image[consider_yxz] = image[consider_yxz] + rand_im_shift
+        rand_im_shift = torch.asarray(rng.uniform(low=0.1, high=0.9, size=n_consider), dtype=image_temp.dtype)
+        image_temp[consider_yxz] = image_temp[consider_yxz] + rand_im_shift
+        consider_intensity_random += rand_im_shift
 
-    consider_intensity = image[consider_yxz]
     consider_yxz = torch.vstack(consider_yxz)
     if (consider_yxz <= np.iinfo(np.int32).max).all():
         consider_yxz = consider_yxz.to(torch.int32)
@@ -81,10 +85,10 @@ def detect_spots(
     n_consider = consider_yxz.shape[1]
     n_shifts = se_shifts.shape[1]
     paddings = (pad_size_z, pad_size_z, pad_size_x, pad_size_x, pad_size_y, pad_size_y)
-    image = torch.nn.functional.pad(image, paddings, mode="constant", value=0)
+    image_temp = torch.nn.functional.pad(image_temp, paddings, mode="constant", value=0)
 
     keep = torch.zeros((n_consider,), dtype=bool, device=run_on)
-    image = image.to(run_on)
+    image_temp = image_temp.to(run_on)
     se_shifts = se_shifts.to(run_on)
 
     # If there are too many potential spots in consider_yxz, then we must run through a subset at a time.
@@ -94,26 +98,26 @@ def detect_spots(
         index_max = min(index_max, n_consider)
         n_consider_batch = index_max - index_min
         consider_yxz_batch = consider_yxz[:, index_min:index_max].to(run_on)
-        consider_intensity_batch = consider_intensity[index_min:index_max].to(run_on)
+        consider_intensity_batch = consider_intensity_random[index_min:index_max].to(run_on)
         # Local pixel positions of spots must change after padding is added
         consider_yxz_se_shifted = (
             consider_yxz_batch + torch.asarray([pad_size_y, pad_size_x, pad_size_z], device=run_on)[:, np.newaxis]
         )
         del consider_yxz_batch
-        # (image.ndim, n_consider, n_shifts) shape
+        # (image_temp.ndim, n_consider, n_shifts) shape
         consider_yxz_se_shifted = torch.repeat_interleave(consider_yxz_se_shifted[..., np.newaxis], n_shifts, dim=2)
         consider_yxz_se_shifted += se_shifts[None].movedim((0, 1, 2), (1, 0, 2))
-        # image.ndim items in tuple of `(n_consider * n_shifts) ndarray[int]`
-        consider_yxz_se_shifted = tuple(consider_yxz_se_shifted.reshape((image.ndim, -1)))
+        # image_temp.ndim items in tuple of `(n_consider * n_shifts) ndarray[int]`
+        consider_yxz_se_shifted = tuple(consider_yxz_se_shifted.reshape((image_temp.ndim, -1)))
         keep_batch = (
-            image[consider_yxz_se_shifted].reshape((n_consider_batch, n_shifts))
+            image_temp[consider_yxz_se_shifted].reshape((n_consider_batch, n_shifts))
             <= consider_intensity_batch[:, np.newaxis]
         ).all(1)
         keep[index_min:index_max] = keep_batch
         del consider_yxz_se_shifted, consider_intensity_batch, keep_batch
 
     keep = keep.cpu()
-    image = image.cpu()
+    image_temp = image_temp.cpu()
 
     peak_intensity = consider_intensity[keep].to(image.dtype)
     peak_yxz = consider_yxz.T[keep]
--- coppafish/find_spots/test/test_find_spots_detect_torch.py
+++ coppafish/find_spots/test/test_find_spots_detect_torch.py
@@ -11,10 +11,28 @@ def test_detect_spots() -> None:
     image[1, 2, 5] = 2
     image[1, 4, 6] = 3
 
-    peak_yxz, peak_intensity = detect_spots(torch.asarray(image).float(), 0.5, 2, 2)
+    for remove_duplicates in (True, False):
+        peak_yxz, peak_intensity = detect_spots(torch.asarray(image), 0.5, 2, 2, remove_duplicates=remove_duplicates)
+        assert peak_yxz.shape == (2, 3)
+        assert torch.allclose(peak_yxz[0], torch.asarray([1, 2, 5])[np.newaxis].int())
+        assert torch.allclose(peak_yxz[1], torch.asarray([1, 4, 6])[np.newaxis].int())
+        assert peak_intensity.shape == (2,)
+        assert torch.allclose(peak_intensity[0], torch.asarray([2]).float())
+        assert torch.allclose(peak_intensity[1], torch.asarray([3]).float())
+
+    # Check that removing duplicate local maxima is working.
+    image_shape = (3, 5, 10)
+    image = np.zeros(image_shape).astype(np.int32)
+    image[1, 2, 5] = 4
+    image[1, 3, 5] = 4
+    peak_yxz, peak_intensity = detect_spots(torch.asarray(image), 3.2, 2, 2, remove_duplicates=False)
     assert peak_yxz.shape == (2, 3)
-    assert torch.allclose(peak_yxz[0], torch.asarray([1, 2, 5])[np.newaxis].int())
-    assert torch.allclose(peak_yxz[1], torch.asarray([1, 4, 6])[np.newaxis].int())
-    assert peak_intensity.shape == (2,)
-    assert torch.allclose(peak_intensity[0], torch.asarray([2]).float())
-    assert torch.allclose(peak_intensity[1], torch.asarray([3]).float())
+    assert torch.allclose(peak_yxz[0], torch.asarray([1, 2, 5]).int())
+    assert torch.allclose(peak_yxz[1], torch.asarray([1, 3, 5]).int())
+    assert torch.allclose(peak_intensity, torch.asarray([4]).int())
+    peak_yxz, peak_intensity = detect_spots(torch.asarray(image), 3.2, 2, 2, remove_duplicates=True)
+    assert peak_yxz.shape == (1, 3)
+    assertion = torch.allclose(peak_yxz[0], torch.asarray([1, 2, 5]).int())
+    assertion = assertion or torch.allclose(peak_yxz[1], torch.asarray([1, 3, 5]).int())
+    assert assertion
+    assert torch.allclose(peak_intensity, torch.asarray([4]).int())
