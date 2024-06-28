6bac04483fc0387442e825834401359da8fda35c
Paul <paul.shuker@outlook.com>
2024-06-28 10:45:44 +0100

    viewer + omp subplots work with new OMP result structure

--- coppafish/omp/base.py
+++ coppafish/omp/base.py
@@ -81,3 +81,29 @@ def get_all_local_yxz(
         all_local_yxz = np.append(all_local_yxz, t_local_yxz, 0)
         all_tiles = np.append(all_tiles, np.full(t_local_yxz.size, t, np.int16))
     return all_local_yxz, all_tiles
+
+
+def get_all_colours(
+    nbp_basic: NotebookPage, nbp_omp: NotebookPage
+) -> Tuple[npt.NDArray[np.float16], npt.NDArray[np.int16]]:
+    """
+    Get spot local positions for every tile, concatenated together.
+
+    Args:
+        - nbp_basic (notebook page): `basic_info` notebook page.
+        - nbp_omp (notebook page): `omp` notebook page.
+
+    Returns:
+        - (`(n_spots) ndarray[int16]`) all_colours: all spot colours.
+        - (`(n_spots) ndarray[int16]`) all_tiles: the tile for each spot.
+    """
+    assert type(nbp_basic) is NotebookPage
+    assert type(nbp_omp) is NotebookPage
+
+    all_colours = np.zeros((0, 3), np.float16)
+    all_tiles = np.zeros(0, np.int16)
+    for t in nbp_basic.use_tiles:
+        t_colours: np.ndarray = nbp_omp.results[f"tile_{t}/colours"][:]
+        all_colours = np.append(all_colours, t_colours, 0)
+        all_tiles = np.append(all_tiles, np.full(t_colours.size, t, np.int16))
+    return all_colours, all_tiles
