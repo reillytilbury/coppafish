9da5e45fb6ac2f5d23855157065fd1e4887fc7a1
Paul <paul.shuker@outlook.com>
2024-07-10 13:15:21 +0100

    score omp unit test fix

--- coppafish/omp/test/test_omp_scores_torch.py
+++ coppafish/omp/test/test_omp_scores_torch.py
@@ -5,7 +5,7 @@ def test_score_coefficient_image() -> None:
     from coppafish.omp import scores_torch
 
     im_y, im_x, im_z = 4, 5, 6
-    spot_shape = 3, 5, 3
+    spot_shape = 1, 3, 5
 
     coefficient_image = torch.zeros((1, im_y, im_x, im_z), dtype=torch.float32)
     coefficient_image[0, 1, 3, 2] = 5
@@ -15,13 +15,14 @@ def test_score_coefficient_image() -> None:
     points[0, 2] = 2
     spot = torch.zeros(spot_shape, dtype=torch.int16)
     mean_spot = torch.zeros(spot_shape, dtype=torch.float32)
-    spot[1, 2, 1] = 1
-    mean_spot[1, 2, 1] = 0.5
-    spot[1, 3, 2] = 1
-    mean_spot[1, 3, 2] = 0.9
+    spot[0, 1, 2] = 1
+    mean_spot[0, 1, 2] = 0.5
+    spot[0, 2, 2] = 1
+    mean_spot[0, 2, 2] = 0.9
+    mean_spot[0, 1, 3] = 10
 
     scores = scores_torch.score_coefficient_image(coefficient_image, spot, mean_spot)
 
     assert scores.shape == coefficient_image.shape
-    assert torch.isclose(scores[0, 1, 3, 2], 5 * 0.5 / ((5) * mean_spot.sum()))
+    assert torch.isclose(scores[0, 1, 3, 2], 5 * 0.5 / mean_spot.sum())
     assert torch.isclose(scores[0, 0, 0, 0], torch.asarray([0], dtype=torch.float32))
