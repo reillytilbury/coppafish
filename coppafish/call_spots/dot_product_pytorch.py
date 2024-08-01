18d7f377c7cfd65af1eba58fc961874ae91c8d82
reilly <reillytilbury@gmail.com>
2024-08-01 13:59:43 +0100

    vectorised dot product pytorch

--- coppafish/call_spots/dot_product_pytorch.py
+++ coppafish/call_spots/dot_product_pytorch.py
@@ -31,18 +31,19 @@ def dot_product_score(
     """
     # If no variance is provided, we assume all spots are equally reliable
     variance = torch.ones_like(spot_colours) if variance is None else variance
+    concentration = torch.sqrt(torch.reciprocal(variance))
+    del variance
+
     # Normalise spot colours
     spot_colours_norm = spot_colours / (torch.linalg.norm(spot_colours, dim=1)[:, None] + norm_shift)
 
-    # z-score spot colours and bled codes by dividing by the standard deviation
-    spot_colours_z_scored = spot_colours_norm / torch.sqrt(variance) # [n_spots, n_r * n_c]
-    # repeat spot colours for each gene
-    spot_colours_z_scored = spot_colours_z_scored[:, None, :].repeat(1, bled_codes.shape[0], 1) # [n_spots, n_genes, n_r * n_c]
-    bled_codes_z_scored = bled_codes[None, :, :] / torch.sqrt(variance)[:, None, :] # [n_spots, n_genes, n_r * n_c]
-
-    # Now we can obtain the dot product score for each spot and each gene
-    all_score = (torch.sum(spot_colours_z_scored * bled_codes_z_scored, dim=2) /
-                 torch.sum(bled_codes_z_scored ** 2, dim=2))
+    # Now we can obtain the dot product score for each spot and each gene (this is done in a vectorised way to avoid
+    # storing intermediate results which would be very large)
+    # score[s, g] = sum_i spot_colours_weighted[s, i] * bled_codes_weighted[g, i] / sum_i bled_codes_weighted[g, i] ** 2
+    # where spot_colours_weighted[s, i] = spot_colours[s, i] / sqrt(variance[s, i]), and similarly
+    # bled_codes_weighted[g, i] = bled_codes[g, i] / sqrt(variance[s, i])
+    all_score = torch.sum(spot_colours_norm[:, None, :] * bled_codes[None, :, :] * concentration[:, None, :] ** 2,
+                          dim=2) / torch.sum(bled_codes[None, :, :] ** 2 * concentration[:, None, :] ** 2, dim=2)
     gene_no = torch.argmax(all_score, dim=1)
     all_score_sorted = torch.sort(all_score, dim=1)[0]
     gene_score = all_score_sorted[:, -1]
