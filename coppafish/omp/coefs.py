c0b0b663ee3ee5b1b2acc82792388612afa375b3
Paul <paul.shuker@outlook.com>
2024-08-09 16:11:59 +0100

    omp compute function + unit test progress

--- coppafish/omp/coefs.py
+++ coppafish/omp/coefs.py
@@ -1,32 +1,41 @@
+import math as maths
 from typing import Tuple
 
 import numpy as np
 import scipy
 import torch
+import tqdm
 
 
 NO_GENE_ASSIGNMENT: int = -32_768
 
 
-def compute_omp(
+def compute_omp_coefficients(
     pixel_colours: np.ndarray[np.float16],
     bled_codes: torch.Tensor,
     background_codes: torch.Tensor,
+    colour_norm_factor: np.ndarray[np.float32],
     maximum_iterations: int,
     dot_product_threshold: float,
     normalisation_shift: float,
     pixel_subset_count: int,
-) -> scipy.sparse.lil_matrix:
+) -> scipy.sparse.spmatrix:
     """
-    Compute OMP coefficients/gene selections from all pixel colours.
+    Compute OMP coefficients for all pixel colours. At each iteration of OMP, the next best gene assignment is found
+    from the residual spot colours divided by their L2 norm + normalisation_shift. A pixel is stopped iterating on if
+    gene assignment fails. See function `get_next_gene_assignments` below for details on the stopping criteria and gene
+    scoring. Pixels that are gene assigned are then fitted with the additional gene to find updated coefficients. See
+    function `get_next_gene_coefficients` for details on the coefficient computation.
 
     Args:
         - pixel_colours (`(n_pixels x n_rounds_use x n_channels_use) ndarray[float16]`): pixel intensity in each
             sequencing round and channel.
-        - bled_codes (`(n_genes x n_rounds_use x n_channels_use) tensor[float]`): every gene bled code.
+        - bled_codes (`(n_genes x n_rounds_use x n_channels_use) tensor[float32]`): every gene bled code.
         - background_codes (`(n_channels_use x n_rounds_use x n_channels_use) tensor[float]`): the background bled
             codes. These are simply uniform brightness in one channel for all rounds. background_codes[0] is the first
             code, background_codes[1] is the second code, etc.
+        - colour_norm_factor (`(1 x n_rounds_use x n_channels_use) ndarray[float32]`): colour scaling applied to each
+            round/channel, calculated during call spots.
         - maximum_iterations (int): the maximum number of gene assignments allowed for one pixel.
         - dot_product_threshold (float): a gene must have a dot product score above this value on the residual spot
             colour to be assigned the gene. If more than one gene is above this threshold, the top score is used.
@@ -39,10 +48,113 @@ def compute_omp(
         (`(n_pixels x n_genes) scipy.sparse.lil_matrix[float32]`) coefficients: each gene coefficient for every pixel.
             Most values will be zero, so kept in a sparse matrix.
     """
-    pass
+    n_pixels, n_rounds_use, n_channels_use = pixel_colours.shape
+    n_rounds_channels_use = n_rounds_use * n_channels_use
+    n_genes = bled_codes.shape[0]
+    assert type(pixel_colours) is np.ndarray
+    assert type(bled_codes) is torch.Tensor
+    assert type(background_codes) is torch.Tensor
+    assert type(colour_norm_factor) is np.ndarray
+    assert type(maximum_iterations) is int
+    assert type(dot_product_threshold) is float
+    assert type(normalisation_shift) is float
+    assert type(pixel_subset_count) is int
+    assert maximum_iterations > 0
+    assert dot_product_threshold >= 0
+    assert normalisation_shift >= 0
+    assert pixel_subset_count > 0
+    assert pixel_colours.ndim == 3
+    assert bled_codes.ndim == 3
+    assert background_codes.ndim == 3
+    assert colour_norm_factor.ndim == 3
+    assert colour_norm_factor.shape[0] == 1
+    assert pixel_colours.size > 0, "pixel_colours cannot be empty"
+    assert bled_codes.numel() > 0, "bled_codes cannot be empty"
+    assert background_codes.numel() > 0, "background_codes cannot be empty"
+    assert colour_norm_factor.size > 0, "colour_norm_factor cannot be empty"
+    assert bled_codes.shape == (n_genes, n_rounds_use, n_channels_use)
+    assert background_codes.shape == (n_channels_use, n_rounds_use, n_channels_use)
+    assert colour_norm_factor.shape == (1, n_rounds_use, n_channels_use)
+
+    all_bled_codes = torch.concat((bled_codes, background_codes), dim=0)
+    all_bled_codes = all_bled_codes.reshape((all_bled_codes.shape[0], n_rounds_channels_use))
+
+    coefficients = scipy.sparse.lil_matrix((n_pixels, n_genes), dtype=np.float32)
+
+    n_subsets = maths.ceil(n_pixels / pixel_subset_count)
+    for i_subset in tqdm.trange(n_subsets, desc=f"Computing OMP Coefficients", unit="subset"):
+        index_min = i_subset * pixel_subset_count
+        index_max = min((i_subset + 1) * pixel_subset_count, n_pixels)
+        subset_size = index_max - index_min
+
+        subset_coefficients = torch.zeros((subset_size, n_genes), dtype=torch.float32)
+        subset_colours = pixel_colours[index_min:index_max].astype(np.float32)
+        subset_colours *= colour_norm_factor
+        subset_colours = torch.tensor(subset_colours)
+        # Flatten colours over rounds/channels.
+        subset_colours = subset_colours.reshape((subset_size, n_rounds_channels_use))
+        # Remember the residual colour between iterations.
+        subset_residual_colours = subset_colours.detach().clone()
+        # Remember what pixels still need iterating on.
+        subset_pixels_to_continue = torch.ones(subset_size, dtype=bool)
+        # Remember the gene selections made for each pixel. NO_GENE_ASSIGNMENT for no gene selection made.
+        subset_genes_selected = torch.full((subset_size, maximum_iterations), NO_GENE_ASSIGNMENT, dtype=torch.int32)
+        bg_gene_indices = torch.linspace(n_genes, n_genes + n_channels_use - 1, n_channels_use, dtype=torch.int32)
+        bg_gene_indices = bg_gene_indices[np.newaxis].repeat_interleave(subset_size, dim=0)
+
+        for iteration in range(maximum_iterations):
+            # The residual colour is L2 normalised + a shift before being used to find the next best gene.
+            subset_residual_colours /= (
+                torch.linalg.vector_norm(subset_colours[subset_pixels_to_continue], dim=1, keepdim=True)
+                + normalisation_shift
+            )
+            # Find the next best gene for pixels that have not reached a stopping criteria yet.
+            fail_gene_indices = torch.cat(
+                (
+                    subset_genes_selected[subset_pixels_to_continue, :iteration],
+                    bg_gene_indices[subset_pixels_to_continue],
+                ),
+                1,
+            )
+            next_best_genes = get_next_gene_assignments(
+                subset_residual_colours,
+                all_bled_codes,
+                fail_gene_indices,
+                dot_product_threshold,
+                maximum_pass_count=maximum_iterations - iteration,
+            )
+            subset_genes_selected[subset_pixels_to_continue, iteration] = next_best_genes
+            del next_best_genes, fail_gene_indices
+
+            # Update what pixels to continue iterating on.
+            subset_pixels_to_continue = subset_genes_selected[:, iteration] != NO_GENE_ASSIGNMENT
+            if subset_pixels_to_continue.sum() == 0:
+                break
+
+            # On the pixels being iterated on, update all the gene coefficients with the new gene added.
+            bled_codes_to_continue = bled_codes[subset_genes_selected[subset_pixels_to_continue, : iteration + 1]]
+            # Flatten rounds/channels.
+            bled_codes_to_continue = bled_codes_to_continue.reshape((-1, iteration + 1, n_rounds_channels_use))
+            # Change to shape (n_pixels_continue, n_rounds_channels_use, iteration + 1).
+            bled_codes_to_continue = bled_codes_to_continue.swapaxes(1, 2)
+            new_coefficients, subset_residual_colours = get_next_gene_coefficients(
+                subset_colours[subset_pixels_to_continue, :, np.newaxis],
+                bled_codes_to_continue,
+            )
+            subset_coefficients[subset_pixels_to_continue] = new_coefficients
+            del new_coefficients, bled_codes_to_continue
+
+        subset_coefficients /= torch.linalg.vector_norm(subset_colours, dim=1, keepdim=True) + normalisation_shift
+        subset_coefficients = subset_coefficients.cpu().numpy()
+
+        # Add the subset coefficients to the sparse coefficients matrix.
+        coefficients[index_min:index_max] = subset_coefficients
+        del subset_coefficients
+
+    return coefficients
 
 
-def get_next_gene_assignment(
+def get_next_gene_assignments(
     residual_colours: torch.Tensor,
     all_bled_codes: torch.Tensor,
     fail_gene_indices: torch.Tensor,
@@ -59,9 +171,9 @@ def get_next_gene_assignment(
 
     The reason for each of these conditions is:
 
-    - Cut out dim background and bad gene reads.
-    - Do not doubly assign a gene and avoid assigning background genes.
-    - Cut out ambiguous colours.
+    - to cut out dim background and bad gene reads.
+    - to not doubly assign a gene and avoid assigning background genes.
+    - to cut out ambiguous colours.
 
     respectively.
 
@@ -136,7 +248,7 @@ def get_next_gene_coefficients(
     Returns:
         - (`(n_pixels x n_genes_added)`) coefficients: the computed coefficients for each gene.
         - (`(n_pixels x n_rounds_channels_use)`) residuals: the residual colour after subtracting off the assigned gene
-            bled code weighted with their coefficients.
+            bled codes weighted with their coefficients.
     """
     assert type(pixel_colours) is torch.Tensor
     assert type(bled_codes) is torch.Tensor
--- coppafish/omp/test/test_omp_coefs.py
+++ coppafish/omp/test/test_omp_coefs.py
@@ -1,9 +1,81 @@
+import numpy as np
+import scipy
 import torch
 
 from coppafish.omp import coefs
 
 
-def test_get_next_gene_assignment() -> None:
+def test_compute_omp_coefficients() -> None:
+    n_pixels = 7
+    n_genes = 2
+    n_rounds_use = 4
+    n_channels_use = 3
+    bled_codes = torch.zeros((n_genes, n_rounds_use, n_channels_use)).float()
+    bled_codes[0, 0, 0] = 1
+    bled_codes[0, 1, 1] = 1
+    bled_codes[1, 0, 1] = 1
+    bled_codes[1, 1, 0] = 1
+    bled_codes[1, 2, 2] = 1
+    pixel_colours = np.zeros((n_pixels, n_rounds_use, n_channels_use), np.float16)
+    # Pixel 0 has no intensity, expecting zero coefficients.
+    pixel_colours[0] = 0
+    # Pixel 1 has only background, expecting zero coefficients.
+    pixel_colours[1, 1] = 2
+    # Pixel 2 has a one strong gene expression weak background.
+    pixel_colours[2] = 1.2 * bled_codes[1].numpy()
+    pixel_colours[2, 0] = 0.02
+    pixel_colours[2, 1] = 0.03
+    # Pixel 3 has a weak gene expression and strong background.
+    pixel_colours[3] = 0.5 * bled_codes[0].numpy()
+    pixel_colours[3, 0] = 2
+    # Pixel 4 has a weak gene expression below the normalisation_shift.
+    pixel_colours[4] = 0.015 * bled_codes[0].numpy()
+    # Pixel 5 has a weak and strong gene expression.
+    pixel_colours[5] = 0.1 * bled_codes[0].numpy() + 2 * bled_codes[1].numpy()
+    # Pixel 6 has both strong gene expressions.
+    pixel_colours[6] = 1.4 * bled_codes[0].numpy() + 2.0 * bled_codes[1].numpy()
+    background_codes = torch.zeros((n_channels_use, n_rounds_use, n_channels_use)).float()
+    background_codes[0, 0] = 1
+    background_codes[1, 1] = 1
+    background_codes[2, 2] = 1
+    colour_norm_factor = np.ones((1, n_rounds_use, n_channels_use), np.float32)
+    colour_norm_factor[0, 3, 2] = 0.1
+    maximum_iterations = 2
+    dot_product_threshold = 0.5
+    normalisation_shift = 0.03
+    pixel_subset_count = 3
+
+    coefficients = coefs.compute_omp_coefficients(
+        pixel_colours=pixel_colours,
+        bled_codes=bled_codes,
+        background_codes=background_codes,
+        colour_norm_factor=colour_norm_factor,
+        maximum_iterations=maximum_iterations,
+        dot_product_threshold=dot_product_threshold,
+        normalisation_shift=normalisation_shift,
+        pixel_subset_count=pixel_subset_count,
+    )
+    assert type(coefficients) is scipy.sparse.lil_matrix
+    assert coefficients.shape == (n_pixels, n_genes)
+    coefficients: np.ndarray = coefficients.toarray()
+    assert np.allclose(coefficients[0], 0)
+    assert np.allclose(coefficients[1], 0)
+    print(coefficients)
+    print(1.2 / np.sqrt(np.square(pixel_colours[2]).sum()))
+    assert np.allclose(coefficients[2], 1.2 / np.sqrt(np.square(pixel_colours[2]).sum()))
+    assert np.allclose(coefficients[0], 0)
+    assert np.allclose(coefficients[0], 0)
+    assert np.allclose(coefficients[0], 0)
+    assert np.allclose(coefficients[0], 0)
+    assert np.allclose(coefficients[0], 0)
+    assert np.allclose(coefficients[0], 0)
+
+
+if __name__ == "__main__":
+    test_compute_omp_coefficients()
+
+
+def test_get_next_gene_assignments() -> None:
     n_pixels = 6
     n_rounds_channels_use = 5
     residual_colours = torch.zeros((n_pixels, n_rounds_channels_use), dtype=torch.float32)
@@ -39,7 +111,7 @@ def test_get_next_gene_assignment() -> None:
     residual_colours_previous = residual_colours.detach().clone()
     all_bled_codes_previous = all_bled_codes.detach().clone()
     fail_gene_indices_previous = fail_gene_indices.detach().clone()
-    best_genes = coefs.get_next_gene_assignment(
+    best_genes = coefs.get_next_gene_assignments(
         residual_colours=residual_colours,
         all_bled_codes=all_bled_codes,
         fail_gene_indices=fail_gene_indices,
@@ -54,7 +126,7 @@ def test_get_next_gene_assignment() -> None:
     assert best_genes[3] == coefs.NO_GENE_ASSIGNMENT
     assert best_genes[4] == coefs.NO_GENE_ASSIGNMENT
     assert best_genes[5] == 1
-    # Since tensors are mutable, check that the inputted tensors have not changed.
+    # Since tensors are mutable, check that the parameter tensors have not changed.
     assert torch.allclose(residual_colours_previous, residual_colours)
     assert torch.allclose(all_bled_codes_previous, all_bled_codes)
     assert torch.allclose(fail_gene_indices_previous, fail_gene_indices)
@@ -75,11 +147,16 @@ def test_get_next_gene_coefficients() -> None:
     pixel_colours[2, 0, 0] = 1 * 0.74
     pixel_colours[2, 1, 0] = 0.4
     pixel_colours[2, 2, 0] = 0.8
+
     bled_codes = torch.zeros((n_pixels, n_rounds_channels_use, n_genes_added))
     bled_codes[:, 0, 0] = 1
     bled_codes[:, 1, 1] = 0.2
     bled_codes[:, 2, 1] = 0.3
+
+    pixel_colours_previous = pixel_colours.detach().clone()
+    bled_codes_previous = bled_codes.detach().clone()
     coefficients, residuals = coefs.get_next_gene_coefficients(pixel_colours=pixel_colours, bled_codes=bled_codes)
+
     assert type(coefficients) is torch.Tensor
     assert type(residuals) is torch.Tensor
     assert coefficients.shape == (n_pixels, n_genes_added)
@@ -95,3 +172,7 @@ def test_get_next_gene_coefficients() -> None:
     assert coefficients[2, 1] > 2
     assert residuals[2, 1] < 0
     assert residuals[2, 2] > 0
+
+    # Since tensors are mutable, check that the parameter tensors have not changed.
+    assert torch.allclose(pixel_colours_previous, pixel_colours)
+    assert torch.allclose(bled_codes_previous, bled_codes)
