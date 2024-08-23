8cfdceafb1cba900f17ea3a5a240de21dd857da9
Paul <paul.shuker@outlook.com>
2024-08-23 14:03:52 +0100

    old import fixed

--- coppafish/plot/call_spots/spot_colours.py
+++ coppafish/plot/call_spots/spot_colours.py
@@ -11,7 +11,6 @@ import numpy as np
 from ...call_spots.dot_product import gene_prob_score
 from ...omp import base as omp_base
 from ...setup import Notebook
-from ...spot_colors import base as spot_colours_base
 from ...spot_colours import base as spot_colours_base
 
 # FIXME: Code outside any functions or classes will slow coppafish importing significantly.
